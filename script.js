e9b5a745daa6b5ce20f5fc56f14dd63e921f444d
DamirPavlik <damir.pavlik@gmail.com>
2025-02-23 18:55:40 +0100

    feat: rotate player around x and y axis

--- script.js
+++ script.js
@@ -50,23 +50,44 @@ class Player {
         /** @type {HTMLImageElement | null} The player sprite image */
         this.image = document.querySelector("#player");
 
+        /** 
+         * @type {[number, number, number, number] | undefined} 
+         * The aim vector representing direction and distance to the target.
+         * [aimX, aimY, dx, dy]:
+         * - `aimX` (number): Normalized x-direction.
+         * - `aimY` (number): Normalized y-direction.
+         * - `dx` (number): Difference in x-coordinates.
+         * - `dy` (number): Difference in y-coordinates.
+         */
         this.aim;
+
+        /** 
+         * @type {number} The player's rotation angle in radians.
+         */
+        this.angle = 0;
     }
 
     /**
      * @param {CanvasRenderingContext2D} context - 2d rendering context of a canvas 
      */
     draw(context) {
-        context.drawImage(this.image, this.x - this.radius, this.y - this.radius);
+        context.save();
+        context.translate(this.x, this.y);
+        context.rotate(this.angle);
+        context.drawImage(this.image, -this.radius, -this.radius);
         context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.arc(0, 0, this.radius, 0, Math.PI * 2);
         context.stroke();
+        context.restore();
     }
 
     update() {
-        this.aim = this.game.calcAim(this.game.mouse, this.game.planet);
+        this.aim = this.game.calcAim(this.game.planet, this.game.mouse);
+
         this.x = this.game.planet.x + (this.game.planet.radius + this.radius) * this.aim[0];
         this.y = this.game.planet.y + (this.game.planet.radius + this.radius) * this.aim[1];
+
+        this.angle = Math.atan2(this.aim[3], this.aim[2]);
     }
 }
 
@@ -115,9 +136,6 @@ class Game {
         this.player.draw(context);
         this.player.update();
         context.beginPath();
-        context.moveTo(this.planet.x, this.planet.y);
-        context.lineTo(this.mouse.x, this.mouse.y);
-        context.stroke();
     }
     
     /**
@@ -134,8 +152,8 @@ class Game {
         const dx = a.x - b.x;
         const dy = a.y - b.y;
         const distance = Math.hypot(dx, dy);
-        const aimX = dx / distance;
-        const aimY = dy / distance;
+        const aimX = dx / distance * -1;
+        const aimY = dy / distance * -1;
 
         return [aimX, aimY, dx, dy];
     }
